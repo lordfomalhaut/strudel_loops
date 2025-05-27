65173a9980b701bf1bd9ec29ee05f2ab1829593e
Marcelo Pérez Boada <marcelo-perezb@unilibre.edu.co>
2025-05-27 17:02:48 -0500

    Update loop_1.js

--- loop_1.js
+++ loop_1.js
@@ -1,3 +1,5 @@
+
+// Loop notes in C minor scale (I think)
 $: note(`
 [f2@2 f2 - g#2 g#2 a#2 g#2 - f2!2 - g#2 f2 a#2 g#2]
 
@@ -10,6 +12,8 @@ $: note(`
   .pianoroll()
   .color("#ff1")
   ._pitchwheel()
+
+// Drums
 $: s("<hh*2>").bank("RolandTR909").gain(.1).room(.3)._pianoroll({labels:1})
 $: s("<- sd>").bank("RolandTR909").gain(.1).room(.3)._pianoroll({labels:1})
 $: s("<- bd>").bank("RolandTR909").gain(1).room(.3)._pianoroll({labels:1})
