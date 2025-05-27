dd4717425f1d3623cb47fb46ce5c50adc29bce24
Marcelo Pérez Boada <marcelo-perezb@unilibre.edu.co>
2025-05-27 18:00:44 -0500

    Update loop_1.js improve drums and add sliders to bass loop

--- loop_1.js
+++ loop_1.js
@@ -5,16 +5,16 @@ setcpm(rate)
 $: note(`
 [f2@2 f2 - g#2 g#2 a#2 g#2 - f2!2 - g#2 f2 a#2 g#2]
 `).sound("gm_synth_bass_2")
-  .lpf(400)
-  .room(.5)
-  .gain(1)
+  .lpf(slider(393.6, 200,1000)) //400
+  .room(slider(0.6, 0, 1)) //0.6
+  .gain(slider(0.7, 0.2, 2)) //0.7
   .cpm(rate/4)
-  .color("cyan magenta")
-  ._pianoroll()
+  .color("violet magenta purple hotpink")
+  ._punchcard()
 
 
 // Drums
-$: s("<hh*2>").bank("RolandCompurhythm1000").gain(.03).room(.2)._pianoroll({labels:1})
-$: s("<oh>/4").bank("RolandTR909").gain(.03).room(.2)._pianoroll({labels:1})
-$: s("<- sd>").bank("RolandTR909").gain(.15).room(.3)._pianoroll({labels:1})
-$: s("<- bd>").bank("RolandTR909").gain(.5).room(.35)._pianoroll({labels:1})
+$: s("<hh>").bank("RolandCompurhythm1000").gain(.03).room(.2).cpm(rate*2)._pianoroll({labels:1})
+$: s("<oh>").bank("RolandTR909").gain(.03).room(.2).cpm(rate/4)._pianoroll({labels:1})
+$: s("<- sd, - sd>").bank("RolandTR909").gain(.15).room(.3).cpm(rate)._pianoroll({labels:1})
+$: s("<- bd>").bank("RolandTR909").gain(.5).room(.35).cpm(rate)._pianoroll({labels:1})
