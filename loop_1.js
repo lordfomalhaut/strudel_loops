8e0fd8f4f8d740870e5f9f9db237849fd390813d
Marcelo Pérez Boada <lordfomalhautdev@gmail.com>
2025-05-29 01:34:12 -0500

    Update loop_1.js added panning

--- loop_1.js
+++ loop_1.js
@@ -5,11 +5,12 @@ setcpm(rate)
 $: note(`
 [f2@2 f2 - g#2!2 a#2 g#2 - f2!2 - g#2 f2 a#2 g#2]
 `).sound("gm_synth_bass_2")
-  .lpf(slider(326.4, 200,1000)) //400
-  .room(slider(0.494, 0, 1)) //0.6
-  .gain(slider(0.227, 0.2, 2)) //0.7
+  .lpf(slider(400, 200,1000)) //400
+  .room(slider(0.6, 0, 1)) //0.6
+  .gain(slider(0.227, 0.2, 2)) //0.2
+  .pan(".4")
   .cpm(rate/4)
-  .color("violet magenta purple hotpink")
+  .color("violet magenta purple orchid")
   .punchcard()
 
 
@@ -23,7 +24,7 @@ $: note(`
   .gain(2)
   .adsr(".1:.5:.8:.1")
   .delay(.5)
-  .pan("0 0.3 .6 1")
+  .pan(".2 .6 .8 1")
   .cpm(rate/6)
   .color("violet magenta purple hotpink")
   ._scope()
@@ -32,5 +33,5 @@ $: note(`
 // Drums
 $: s("<hh>").bank("RolandCompurhythm1000").gain(.03).room(.2).cpm(rate*2)._pianoroll({labels:1})
 $: s("<oh>").bank("RolandTR909").gain(.03).room(.2).cpm(rate/4)._pianoroll({labels:1})
-$: s("<- sd, - sd>").bank("RolandTR909").gain(.08).room(.3).cpm(rate)._pianoroll({labels:1})
+$: s("<- sd, - sd>").bank("RolandTR909").gain(.08).room(.3).pan(".6").cpm(rate)._pianoroll({labels:1})
 $: s("<- bd>").bank("RolandTR909").gain(.25).room(.35).cpm(rate)._pianoroll({labels:1})
