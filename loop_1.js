f6e548f383002ec155a34a42b9ab2b5784581207
Marcelo Pérez Boada <lordfomalhautdev@gmail.com>
2025-05-29 02:07:48 -0500

    Update loop_1.js adding some variables

--- loop_1.js
+++ loop_1.js
@@ -1,37 +1,41 @@
 let rate = 80
 setcpm(rate)
 
+const dbank = "RolandTR909"
+const hhbank = "RolandCompurhythm1000"
+const bassSound = "gm_synth_bass_2"
+const choirSounds = "gm_choir_aahs:2, saw"
+
+const colors = "violet magenta purple hotpink"
+
 // Bass loop notes in C minor scale (I think)
 $: note(`
 [f2@2 f2 - g#2!2 a#2 g#2 - f2!2 - g#2 f2 a#2 g#2]
-`).sound("gm_synth_bass_2")
+`).sound(bassSound)
   .lpf(slider(400, 200,1000)) //400
   .room(slider(0.6, 0, 1)) //0.6
   .gain(slider(0.227, 0.2, 2)) //0.2
   .pan(".4")
   .cpm(rate/4)
-  .color("violet magenta purple orchid")
+  .color(colors)
   .punchcard()
 
 
 // choirs?
 $: note(`
-[f4@2 g#4@2 a#4@2 f4@2 - - - -]
-`).sound("gm_choir_aahs:2, saw").n("<32>").phaser(4).vowel("<i>")
+[-@2 f4@2 g#4@2 a#4@2 f4@2 -@2]
+`).sound(choirSounds).n("<32>").phaser(4).vowel("<i>")
   .lpf(slider(593.6, 200,1000)) //400
   .room(slider(0.598, 0, 1)) //0.6
-  // .gain(slider(2, 0.2, 2)) //0.7
   .gain(2)
   .adsr(".1:.5:.8:.1")
   .delay(.5)
-  .pan(".2 .6 .8 1")
+  .pan(".2 .4 .6 .8 1")
   .cpm(rate/6)
-  .color("violet magenta purple hotpink")
-  ._scope()
 
 
 // Drums
-$: s("<hh>").bank("RolandCompurhythm1000").gain(.03).room(.2).cpm(rate*2)._pianoroll({labels:1})
-$: s("<oh>").bank("RolandTR909").gain(.03).room(.2).cpm(rate/4)._pianoroll({labels:1})
-$: s("<- sd, - sd>").bank("RolandTR909").gain(.08).room(.3).pan(".6").cpm(rate)._pianoroll({labels:1})
-$: s("<- bd>").bank("RolandTR909").gain(.25).room(.35).cpm(rate)._pianoroll({labels:1})
+$: s("<hh>").bank(hhbank).gain(.03).room(.2).cpm(rate*2)._pianoroll({labels:1})
+$: s("<oh>").bank(dbank).gain(.03).room(.2).cpm(rate/4)._pianoroll({labels:1})
+$: s("<- sd, - sd>").bank(dbank).gain(.08).room(.3).pan(".6").cpm(rate)._pianoroll({labels:1})
+$: s("<- bd>").bank(dbank).gain(.25).room(.35).cpm(rate)._pianoroll({labels:1})
